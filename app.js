7699f21e30c6d2956935d3bdac51ce2ea85e4cf0
Rexford Wiafe <w.rexford@yahoo.com>
2022-06-15 16:54:27 -0500

    create a program to implement a number guessing game

--- app.js
+++ app.js
@@ -1,2 +1,37 @@
-console.log("HELLO PAPA PLATOON!")
-// Your function(s) should go here that will interact with the webpage or DOM
+let answer = Math.floor(Math.random() * 100 + 1);
+let isSolved = false;
+let count = 0;
+let userGuess;
+let response = "";
+
+function reset() {
+  answer = Math.floor(Math.random() * 100 + 1);
+  isSolved = false;
+  count = 0;
+  userGuess = "";
+  response = "";
+  document.querySelector("#guessCount").innerHTML = "";
+  document.querySelector("#response").innerHTML = "";
+  document.querySelector("#userGuess").value = "";
+}
+
+function GuessingGame(e) {
+  e.preventDefault();
+  userGuess = Number(document.querySelector("#userGuess").value);
+  count += 1;
+  document.querySelector(
+    "#guessCount"
+  ).innerHTML = `Number of guesses: ${count}`;
+  if (userGuess > answer) {
+    response = "Too High!";
+  } else if (userGuess < answer) {
+    response = "Too Low!";
+  } else if (userGuess === answer) {
+    response = "Congratulations, You guessed the correct Number!";
+    document.querySelector("#restart").style.display = "block";
+    document.querySelector("#mainDiv").style.backgroundImage =
+      "url('./images/carlton.gif')";
+  }
+  document.querySelector("#userGuess").value = "";
+  document.querySelector("#response").innerHTML = response;
+}
